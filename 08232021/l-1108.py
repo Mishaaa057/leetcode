9ce5f2025bf2dc28a1675d82f689e51cde2d7ab7
Misha <kukubik.02@gmail.com>
2021-08-23 21:13:53 -0700

    [08232021] - args added

--- 08232021/l-1108.py
+++ 08232021/l-1108.py
@@ -1,3 +1,19 @@
+import sys
+
+argumentlist = sys.argv[1:]
+first = argumentlist[0]
+
+if first == '-d':
+    print("Given a valid (IPv4) address, returned defanged version of of that IP address")
+
+elif first == '--help' or first == '-h':
+    print('[-d] - description\n[-e] - one example how to use it\n[-args] - arguments throught comma')
+
+elif first == '-e':
+    print('Input: 0.0.0.1\nOutput: 0[.]0[.]0[.]1')
+
+
+
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         string = ''
@@ -8,3 +24,16 @@ class Solution:
                 string += el
                 
         return string
+
+if first == '-args':
+    for el in argumentlist[1:]:
+        result = (Solution().defangIPaddr(el))
+        print(f'Input: {el}')
+        print(f'Output: {result}')
+
+
+# Input: "0.0.0.1"
+# Output: "0[.]0[.]0[.]1"
+
+# Input "0.0"
+# Output: "0[.]0"
