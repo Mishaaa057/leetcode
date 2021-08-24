e66385989f88d08a87dfd7725f78a2bfeeaed0d0
Mariia Paniutina <yangol89@gmail.com>
2021-08-23 21:23:49 -0700

    Added a main function

--- 08232021/l-1108.py
+++ 08232021/l-1108.py
@@ -1,19 +1,5 @@
 import sys
 
-argumentlist = sys.argv[1:]
-first = argumentlist[0]
-
-if first == '-d':
-    print("Given a valid (IPv4) address, returned defanged version of of that IP address")
-
-elif first == '--help' or first == '-h':
-    print('[-d] - description\n[-e] - one example how to use it\n[-args] - arguments throught comma')
-
-elif first == '-e':
-    print('Input: 0.0.0.1\nOutput: 0[.]0[.]0[.]1')
-
-
-
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         string = ''
@@ -25,12 +11,27 @@ class Solution:
                 
         return string
 
-if first == '-args':
-    for el in argumentlist[1:]:
-        result = (Solution().defangIPaddr(el))
-        print(f'Input: {el}')
-        print(f'Output: {result}')
+def Main():
+    argumentlist = sys.argv[1:]
+    first = argumentlist[0]
 
+    if first == '-d':
+        print("Given a valid (IPv4) address, returned defanged version of of that IP address")
+
+    elif first == '--help' or first == '-h':
+        print('[-d] - description\n[-e] - one example how to use it\n[-args] - arguments throught comma')
+
+    elif first == '-e':
+        print('Input: 0.0.0.1\nOutput: 0[.]0[.]0[.]1')
+
+    if first == '-args':
+        for el in argumentlist[1:]:
+            result = (Solution().defangIPaddr(el))
+            print(f'Input: {el}')
+            print(f'Output: {result}')
+
+if __name__ == "__main__":
+    Main()
 
 # Input: "0.0.0.1"
 # Output: "0[.]0[.]0[.]1"
