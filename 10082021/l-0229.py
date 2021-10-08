43ee3fa4aa9d7b3f23199e8f16fb8344475d462c
Mishaaa057 <kukubik.02@gmail.com>
2021-10-08 12:04:38 +0300

    [10082021] :: l-0229 is done

--- 10082021/l-0229.py
+++ 10082021/l-0229.py
@@ -1,4 +1,4 @@
-# 169. Majority Element
+# 229. Majority Element II
 
 
 import argparse
@@ -8,7 +8,7 @@ from colorama import Fore
 def BuildArgParser(descr):
 
 	parser = argparse.ArgumentParser(descr)
-	parser.add_argument('-n', '--nums', type=int, metavar='n',nargs='+', help='Nums array')
+	parser.add_argument('-n', '--nums', type=int, metavar='n', nargs='+', help='Nums array')
 	parser.add_argument('-d', '--description', action='store_true', help='Show description')
 	parser.add_argument('-e', '--example', action='store_true', help='Show example')
 
@@ -16,7 +16,7 @@ def BuildArgParser(descr):
 
 
 class Solution:
-    def majorityElement(self, nums: list) -> int:
+    def majorityElement(self, nums: list) -> list:
         dct = {}
         
         for el in nums:
@@ -24,36 +24,40 @@ class Solution:
                 dct[el] = 1
             else:
                 dct[el] += 1
-            
+        
+        result = []
+        
         for key in dct:
-            if dct[key] > len(nums) / 2:
-                return key
+            if dct[key] > len(nums) / 3:
+                result.append(key)
+        
+        return result
 
 
 def main():
 	descr = f'''
 {Fore.RED}=========================================================={Fore.RESET}
 {Fore.GREEN}
-	#  169. Majority Element
+	#  229. Majority Element II
 
-Given an array nums of size n, return the majority element.
-
-The majority element is the element that appears more than 
-⌊n / 2⌋ times. You may assume that the majority element 
-always exists in the array.
+Given an integer array of size n, find all elements 
+that appear more than ⌊ n/3 ⌋ times.
 {Fore.RESET}
 {Fore.RED}=========================================================={Fore.RESET}
 	'''
 
 	example = '''
 Example 1:
-	$ python3 l-0169.py --nums 3 2 3
-	>>> 3
+	$ python3 l-0229.py --nums 3 2 3
+	>>> [3]
 
 Example 2:
+	$ python3 l-0229.py --nums 1
+	>>> [1]
 
-	$ python3 l-0169.py --nums 2 2 1 1 1 2 2
-	>>> 2
+Example 3:
+	$ python3 l-0229.py --nums 1 2
+	>>> [1,2]
 	'''
 
 	parser = BuildArgParser(descr)
